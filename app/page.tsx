c5b13559aca8139cb2d0ff5fd763fca149fe2acf
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-11-13 22:25:14 +0200

    Auth Clerk setup

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Hello World</h1>
-      <h2 className="h2-bold">Hello World</h2>
-      <h3 className="h3-bold">Hello World</h3>
-    </div>
-  );
-}
